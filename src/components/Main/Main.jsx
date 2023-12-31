a1fa6a3706c44171e5a4be674f7f57686b5688ca
velisssa <tybinka.vasylyna@gmail.com>
2023-12-31 01:02:10 +0100

    Add pagination

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,10 +2,13 @@ import React, {useState} from 'react';
 import "./Main.scss"
 import customers, {user} from "../../assets/data";
 import {DataGrid} from '@mui/x-data-grid';
+import {Pagination, Stack} from "@mui/material";
 
 
 const Main = () => {
     const [searchValue, setSearchValue] = useState("");
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const PAGE_SIZE = 8;
 
     const handleSearchChange = (event) => {
         setSearchValue(event.target.value);
@@ -20,19 +23,27 @@ const Main = () => {
         customer.customerName.toLowerCase().includes(searchValue.toLowerCase())
     );
 
+    const [result, setResult] = useState([...filteredCustomers]);
+
     const columns = [
-        {field: 'customerName', headerName: 'Customer Name', flex: 1},
+        {field: 'customerName', headerName: 'Customer Name', flex: 1.5},
         {field: 'company', headerName: 'Company', flex: 1},
         {field: 'phoneNumber', headerName: 'Phone Number', flex: 1},
-        {field: 'email', headerName: 'Email', flex: 1},
+        {field: 'email', headerName: 'Email', flex: 1.5},
         {field: 'country', headerName: 'Country', flex: 1},
         {
             field: 'status',
             headerName: 'Status',
-            flex: 1,
             renderCell: (params) => renderStatusButton(params.value),
         },
     ];
+
+    const handlePageChange = (event, newPage) => {
+        setCurrentPage(newPage);
+        const startIndex = (newPage - 1) * PAGE_SIZE;
+        const endIndex = startIndex + PAGE_SIZE;
+        setResult(filteredCustomers.slice(startIndex, endIndex))
+    };
 
     return (
         <main className="main">
@@ -63,15 +74,34 @@ const Main = () => {
                 </div>
 
                 <div className="customers__table" style={{height: 'auto', width: '100%'}}>
-                    <DataGrid
-                        rows={filteredCustomers}
-                        columns={columns}
-                        initialState={{
-                            pagination: {
-                                paginationModel: {page: 0, pageSize: 8},
-                            },
-                        }}
-                    />
+
+                    {result.length > 0
+                        ? <>
+                            <DataGrid
+                                rows={result}
+                                columns={columns}
+                                initialState={{
+                                    pagination: {
+                                        paginationModel: {page: 0, pageSize: PAGE_SIZE},
+                                    },
+                                }}
+                            />
+
+                            <Stack >
+                                <div className="pagination__info">{`Showing data ${(currentPage - 1) * PAGE_SIZE + 1} to ${Math.min(currentPage * PAGE_SIZE, filteredCustomers.length)} of ${filteredCustomers.length} entries`}</div>
+                                <div className="pagination__pages">
+                                    <Pagination
+                                        count={Math.ceil(filteredCustomers.length / PAGE_SIZE)}
+                                        page={currentPage}
+                                        onChange={handlePageChange}
+                                    />
+                                </div>
+
+                            </Stack>
+                        </>
+                        : <div>No data found</div>
+                    }
+
                 </div>
             </div>
         </main>
